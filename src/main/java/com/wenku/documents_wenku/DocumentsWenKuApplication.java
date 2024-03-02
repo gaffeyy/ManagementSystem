d18c1f04c6efe095f5016470ce846a68d8a31750
gaffey <Songyjgaf@163.com>
2024-03-02 17:25:33 +0800

    Initial Commit

---
+++ src/main/java/com/wenku/documents_wenku/DocumentsWenKuApplication.java
@@ -0,0 +1,13 @@
+package com.wenku.documents_wenku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsWenKuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentsWenKuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wenku/documents_wenku/DocumentsWenKuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenku.documents_wenku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentsWenKuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
